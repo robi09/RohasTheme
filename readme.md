bc8fbceab9aed4a903b1e522b1ae0a3250028930
Hapiuc Robert <robert@9pixels.co>
2016-02-07 21:30:11 +0200

    html files done

---
+++ readme.md
@@ -0,0 +1,2 @@
+http://subtlepatterns.com/concrete-seamless/
+http://subtlepatterns.com/concrete-wall/
---
+++ single.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>RohasTheme</title>
+</head>
+<body>
+	<header id="header">
+		<div id="logo">
+			<h1>RohasTheme</h1>
+			<h2>Clean WordPress blog theme</h2>
+		</div><!-- /#logo  -->
+	</header><!-- /#header  -->
+	<nav id="main_navigation">
+		<div class="wrapper">
+			<ul>
+				<li><a href="">Lipsum 1</a></li>
+				<li><a href="">Lipsum 2</a></li>
+				<li><a href="">Lipsum 3</a></li>
+				<li><a href="">Lipsum 4</a></li>
+				<li><a href="">Lipsum 5</a></li>
+			</ul>
+		</div><!-- / .wrapper -->
+	</nav><!-- /#main_navigation  -->
+
+	<main>
+		<div class="wrapper">
+			<article class="post single-post">
+				<figure class="featured_image"></figure><!-- / .featured_image -->
+				<div class="post_content">
+					<h3><a href="">Post title test 1</a></h3>
+					<div class="post_meta"></div><!-- / .post_meta -->
+					
+					<div class="content">
+						
+					</div><!-- / .content -->
+				</div><!-- / .post_content -->
+			</article><!-- / .post -->
+		</div><!-- / .wrapper -->
+	</main>
+	<footer id="footer">
+		<div class="wrapper">
+			<div class="widget">
+				<div class="widget_title"></div><!-- / .widget_title -->
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, asperiores, molestias. Blanditiis, exercitationem, illum. Ullam modi consectetur quos natus commodi nam tenetur eligendi tempore libero, corporis, nesciunt, recusandae inventore provident.</p>
+			</div><!-- / .widget -->
+			<div class="widget">
+				<div class="widget_title"></div><!-- / .widget_title -->
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit voluptates, vero suscipit alias repellendus in ab officiis ipsum amet molestias eveniet optio laudantium veniam asperiores quidem! Optio repellat iure voluptatibus?</p>
+			</div><!-- / .widget -->
+			<div class="widget">
+				<div class="widget_title"></div><!-- / .widget_title -->
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis blanditiis quis repellat nisi dolorum reprehenderit iure, officiis qui est voluptatem ad quibusdam neque necessitatibus delectus quod, voluptate id illum vel.</p>
+			</div><!-- / .widget -->
+		</div><!-- / .wrapper -->
+		<div class="copyright"></div><!-- / .copyright -->
+	</footer><!-- /#footer  -->
+</body>
+</html>
