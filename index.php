39cd7996ba2952a6f1eb17102bae2a044d5979a0
Hapiuc Robert <robert@9pixels.co>
2016-02-24 14:06:27 +0200

    things

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<?php 
+
+get_header();
+
+if ( have_posts() ) : 
+
+	while ( have_posts() ) : the_post();
+
+		get_template_part( 'parts/content-post' );
+
+	endwhile;
+
+	// Previous/next page navigation.
+	the_posts_pagination( array(
+		'prev_text'          => __( 'Previous page', 'rohas' ),
+		'next_text'          => __( 'Next page', 'rohas' ),
+	) );
+
+else :
+	get_template_part( 'parts/content', 'none' );
+endif;
+
+get_footer(); 
+
+?>
